2feaf533401b52506dab25085cda5147cedba526
securelyshare-swathi-bali <bali.swathi@securelyshare.com>
2022-06-25 21:53:53 +0530

    add css loader

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import "./popup.css";
+import "./options.css";
 
 const test = <img src="icon.png" alt="react" />;
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   devtool: "cheap-module-source-map",
   entry: {
     popup: path.resolve("src/popup/popup.tsx"),
+    options: path.resolve("src/options/options.tsx"),
   },
   module: {
     rules: [
@@ -15,22 +16,26 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
       },
+      {
+        use: ["style-loader", "css-loader"],
+        test: /\.css?$/i,
+      },
+      {
+        type: "asset/resource",
+        test: /\.(jpg|jpeg|png|woff|woff2|e0t|ttf|svg)$/,
+      },
     ],
   },
   plugins: [
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve("src/manifest.json"),
+          from: path.resolve("src/static"),
           to: path.resolve("dist"),
         },
       ],
     }),
-    new HtmlPlugin({
-      title: "React Extension",
-      filename: "popup.html",
-      chunks: ["popup"],
-    }),
+    ...getHtmlPlugin(["popup", "options"]),
   ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
@@ -39,4 +44,20 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve("dist"),
   },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 };
+
+function getHtmlPlugin(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: "React Extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+}
